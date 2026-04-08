3c48849d67a74fb13478d03ee012a26e693233f5
Henri <henrikarba@gmail.com>
2026-04-08 21:50:01 +0300

    404 fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 
 import path from 'path';
+import fs from 'fs';
 
 
 
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
 const BUILD_DIR = path.resolve(__dirname, './dist',);
+
+// Plugin to copy index.html to 404.html after build
+const copy404Plugin = {
+  name: 'copy-404',
+  writeBundle() {
+    const indexPath = path.join(BUILD_DIR, 'index.html');
+    const notFoundPath = path.join(BUILD_DIR, '404.html');
+    if (fs.existsSync(indexPath)) {
+      fs.copyFileSync(indexPath, notFoundPath);
+    }
+  }
+};
+
 export default async () => {
   const { svelte } = await import('@sveltejs/vite-plugin-svelte');
   return {
     plugins: [
       svelte(),
+      copy404Plugin,
 
     ],
     root: SRC_DIR,
